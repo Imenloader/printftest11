ed379e73b0e7e15700bd13c40afa9d4a1402084e
Imenloader <greenfeeda@gmail.com>
2023-10-17 03:13:03 -0700

    5

--- _printf.c
+++ _printf.c
@@ -1,56 +1,80 @@
 #include <stdio.h>
-#include <main.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
+* _printf_char - Print a character.
+* @list: Variable argument list.
+* Return: Number of characters printed.
+*/
+int _printf_char(va_list *list)
+{
+char character = va_arg(*list, int);
+write(1, &character, 1);
+return (1);
+}
+
+/**
+* _printf_string - Print a string.
+* @list: Variable argument list.
+* Return: Number of characters printed.
+*/
+int _printf_string(va_list *list)
+{
+char *string = va_arg(*list, char *);
+write(1, string, strlen(string));
+return (strlen(string));
+}
+
+/**
+* _printf - Printf function.
+* @format: Format string.
+* ...: Variable arguments.
+* Return: Number of characters printed.
+*/
 int _printf(const char *format, ...)
 {
-int Chars_Print = 0;
-va_list List_Of_Arguments;
-if (format == NULL)
-{
-return (-1);
-}
-va_start(List_Of_Arguments, format);
+int characters_printed = 0;
+va_list list;
+va_start(list, format);
 while (*format)
 {
 if (*format != '%')
 {
 write(1, format, 1);
-Chars_Print++;
+characters_printed++;
 }
 else
-format++;
-if (format == '\0')
 {
+format++;
+switch (*format)
+{
+case '%':
+write(1, format, 1);
+characters_printed++;
+break;
+case 'c':
+characters_printed += _printf_char(&list);
+break;
+case 's':
+characters_printed += _printf_string(&list);
 break;
 }
-if (*format == '%')
-{
-write(1, format, 1);
-Chars_Print++;
-}
-else if (*format == 'c')
-{
-char Charactera = va_arg(List_Of_Arguments, int);
-write(1, &Charactera, 1);
-Chars_Print++;
-}
-else if (*format == 's')
-{
-char *infinty_strings = va_arg(List_Of_Arguments, char *);
-int infinty_strings_len = 0;
-while (infinty_strings[infinty_strings_len] != '\0')
-{
-infinty_strings_len++;
-}
-write(1, infinty_strings, infinty_strings_len);
-Chars_Print += infinty_strings_len;
 }
 format++;
 }
-va_end(List_Of_Arguments);
-return (Chars_Print);
+va_end(list);
+return (characters_printed);
+}
+
+/**
+* main - Entry point for the program.
+* @void: No arguments.
+* Return: 0 on success.
+*/
+int main(void)
+{
+_printf("Hello, world!\n");
+return (0);
 }
