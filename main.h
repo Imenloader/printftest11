23e3f2fd099f3cf565c4d7dbe134d08e4401db9c
Imenloader <greenfeeda@gmail.com>
2023-10-17 03:32:53 -0700

    5

--- main.h
+++ main.h
@@ -5,7 +5,11 @@
 #include <unistd.h>
 #include <string.h>
 
+/*Mandatory functions*/
 int _printf(const char *format, ...);
+int op_ch(va_list arg);
+int op_st(va_list arg);
+int op_nu(va_list arg);
 
 
 #endif /* MAIN_H */
